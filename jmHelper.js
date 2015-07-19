5fc6f8f4b9ff9d60cfcfb42cabddf6e4780cf9dd
Jose Cerrejon <ulysess@gmail.com>
2015-07-19 12:09:30 +0200

    Added listViewJSon

--- jmHelper.js
+++ jmHelper.js
@@ -48,3 +48,27 @@ exports.postData = function(url, obj, _callback) {
     xhr.open("POST", url);
     xhr.send(param);
 };
+
+// jSON example: [{"title":"Hydrogen","subtitle": "H"},{"title":"Helium","subtitle": "He" }]
+// URL: http://misapuntesde.com/listview_ex.json
+
+exports.listViewJSon = function(url, obj) {
+	this.getData(url, function(_response) {
+		if (_response.success) {
+			var items = _.map(_response.data, function(element) {
+				return {
+					properties : {
+						title : element.title,
+						subtitle : element.subtitle || ""
+					}
+				};
+			});
+
+			if (obj.section==undefined){
+				var listSection = Ti.UI.createListSection();
+ 				obj.appendSection(listSection);
+ 			}
+			obj.sections[0].setItems(items);
+		}
+	});
+};
